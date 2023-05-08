082f959790789ede395b74f46d20cc80be73b69d
mohsen tavosi <tavosi2013@gmail.com>
2023-05-08 11:46:47 +0330

    decrease font size to 16.

--- lib/conf/laid_back_config.dart
+++ lib/conf/laid_back_config.dart
@@ -6,4 +6,16 @@ class LaidBackConfig {
   Locale? locale;
 
   LaidBackConfig({this.clientType, this.textStyle, this.locale});
+
+  static init({
+    String? clientType,
+    TextStyle? textStyle,
+    Locale? locale,
+  }) async {
+    LaidBackConfig(
+      clientType: clientType,
+      locale: locale,
+      textStyle: textStyle,
+    );
+  }
 }
